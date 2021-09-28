05dd7a6bd991688b4cd0e4fd2b6b681c5d90c6e5
fikaduastu <1988.fikadufilipos@gmail.com>
2021-09-27 22:13:13 -0500

    db connection module added

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const conn = require('./db-connection');
 const app = express();
+
 const port = process.env.port || 5000;
 
+conn.db.connect((error)=>{
+    if (error){
+    console.log(error);
+    }
+    else{
+       console.log("conneted success");
+    }
+});
 app.get('/',(req,res)=>{
     res.send("<h1> This is Homepage</h1>");
 });
